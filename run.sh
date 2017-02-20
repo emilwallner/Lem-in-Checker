002ffdb2a9182e74b41e3aeb760e92bde924d7a7
Emil Wallner <emilw@llner.co>
2017-02-20 19:25:18 +0100

    m

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-GREEN="\n\n\033[32m";
+GREEN="\033[32m";
 RED="\033[31m";
-DEFAULT=""\033[37m";
-CYAN=""\x1b[36m";
+DEFAULT="\033[37m";
+CYAN="\x1b[36m";
 USER="Result/";
 P="./lem-in";
 DIR="Maps/"
@@ -37,44 +37,50 @@ FILE18="err_letter_in_coordinate.map";
 FILE19="err_no_path.map";
 
 echo "\n\n\033[32m$FILE\n\033[37m"
-$C $DIR$FILE | $P > $USER$FILE;
+$C $DIR$FILE | $P > $USER$FILE | leaks lem-in;
+echo "\033[37mOutput: cat $USER$FILE\n"
 echo "\n\n\033[32m$FILE2\n\033[37m"
-$C $FILE2 | $P > $USER$FILE2;
-echo "\n\n\n\033[32m$FILE3\n\033[37m"
-$C $DIR$FILE3 | $P > $USER$FILE3;
+$C $FILE2 | $P > $USER$FILE2 | leaks lem-in;
+echo "\033[37mOutput: cat $USER$FILE2\n"
 echo "\n\n\033[32m$FILE4\n\033[37m"
-$C $DIR$FILE4 | $P > $USER$FILE4;
+$C $DIR$FILE3 | $P > $USER$FILE3 | leaks lem-in;
+echo "\033[37mOutput: cat $USER$FILE4\n"
+# echo "\n\n\033[32m$FILE4\n\033[37m"
+# $C $DIR$FILE4 | $P > $USER$FILE4;
+# echo "\033[37mOutput: cat $USER$FILE4\n"
 echo "\n\n\033[32m$FILE7\n\033[37m"
-$C $DIR$FILE7 | $P > $USER$FILE7;
+$C $DIR$FILE7 | $P > $USER$FILE7 | leaks lem-in;
+echo "\033[37mOutput: cat $USER$FILE7\n"
 echo "\n\n\033[32m$FILE8\n\033[37m"
-$C $DIR$FILE8 | $P > $USER$FILE8;
+$C $DIR$FILE8 | $P > $USER$FILE8 | leaks lem-in;
+echo "\033[37mOutput: cat $USER$FILE8\n"
 echo "\n\n\033[32m$FILE9\n\033[37m"
-$C $DIR$FILE9 | $P > $USER$FILE9;
+$C $DIR$FILE9 | $P > $USER$FILE9 | leaks lem-in;
 echo "\n\n\033[32m$FILE10\n\033[37m"
-$C $DIR$FILE10 | $P > $USER$FILE10;
+$C $DIR$FILE10 | $P > $USER$FILE10 | leaks lem-in;
 echo "\n\n\033[32m$FILE11\n\033[37m"
-$C $DIR$FILE11 | $P > $USER$FILE11;
+$C $DIR$FILE11 | $P > $USER$FILE11 | leaks lem-in;
 echo "\n\n\033[32m$FILE12\n\033[37m"
-$C $DIR$FILE12 | $P > $USER$FILE12;
+$C $DIR$FILE12 | $P > $USER$FILE12 | leaks lem-in;
 echo "\n\n\033[32m$FILE13\n\033[37m"
-$C $DIR$FILE13 | $P > $USER$FILE13;
+$C $DIR$FILE13 | $P > $USER$FILE13 | leaks lem-in;
 echo "\n\n\033[32m$FILE14\n\033[37m"
-$C $DIR$FILE14 | $P > $USER$FILE14;
+$C $DIR$FILE14 | $P > $USER$FILE14 | leaks lem-in;
 echo "\n\n\033[32m$FILE15\n\033[37m"
-$C $DIR$FILE15 | $P > $USER$FILE15;
+$C $DIR$FILE15 | $P > $USER$FILE15 | leaks lem-in;
 echo "\n\n\033[32m$FILE16\n\033[37m"
-$C $DIR$FILE16 | $P > $USER$FILE16;
+$C $DIR$FILE16 | $P > $USER$FILE16 | leaks lem-in;
 echo "\n\n\033[32m$FILE17\n\033[37m"
-$C $DIR$FILE17 | $P > $USER$FILE17;
+$C $DIR$FILE17 | $P > $USER$FILE17 | leaks lem-in;
 echo "\n\n\033[32m$FILE18\n\033[37m"
-$C $DIR$FILE18 | $P > $USER$FILE18;
+$C $DIR$FILE18 | $P > $USER$FILE18 | leaks lem-in;
 echo "\n\n\033[32m$FILE19\n\033[37m"
-$C $DIR$FILE19 | $P > $USER$FILE19;
+$C $DIR$FILE19 | $P > $USER$FILE19 | leaks lem-in;
 echo "\n\n\033[32m/dev/urandom/\033[37m"
-$C /dev/urandom | $P > $USER-dev-urandom;
+$C /dev/urandom | $P > $USER-dev-urandom | leaks lem-in;
 echo "\n\n\033[32m/dev/null/\033[37m"
-$C /dev/null | $P > $USER-dev-null;
+$C /dev/null | $P > $USER-dev-null | leaks lem-in;
 echo "\n\n\033[32m/dev/zero/\033[37m"
-$C /dev/zero | $P > $USER-dev-zero;
+$C /dev/zero | $P > $USER-dev-zero | leaks lem-in;
 echo "\n\n\033[32mNothing\033[37m"
-echo "" | $P > $USER-Nothing;
+echo "" | $P > $USER-Nothing | leaks lem-in;
