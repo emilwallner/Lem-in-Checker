9a750b359aa328877eb922197c17f08266c679c6
Emil Wallner <w@llner.co>
2017-02-22 15:23:50 +0100

    7 more errors

--- run.sh
+++ run.sh
@@ -22,9 +22,9 @@ FILE2="2.map";
 FILE3="5.map";
 FILE4="10K.map";
 FILE5="100K.map";
-FILE6="err_no_end.map";
 FILE7="10_lots_of_comments.map";
 FILE8="20.map";
+FILE6="err_no_end.map";
 FILE9="err_3_coordinates.map";
 FILE10="err_duplicate.map";
 FILE11="err_no_ants2.map";
@@ -35,21 +35,21 @@ FILE15="err_empty.map";
 FILE16="err_no_ants3.map";
 FILE17="err_no_rooms.map";
 FILE18="err_letter_in_coordinate.map";
-FILE10="err_link_to_unknown_room.map";
-FILE11="err_no_end2.map";
-FILE12="err_no_start2.map";
-FILE13="err_start_end_same.map";
-FILE14="err_too_many_ants.map";
-FILE15="err_no_start.map";
-FILE16="err_no_ants3.map";
-FILE17="err_no_rooms.map";
-FILE18="err_letter_in_coordinate.map";
 FILE19="err_no_path.map";
 FILE20="9_path_depth_2_ants.map";
 FILE24="9_path_depth_1_ants.map";
 FILE21="9_path_depth_8_ants.map";
 FILE22="9_path_depth_9_ants.map";
 FILE23="9_path_depth_10_ants.map";
+FILE19="err_link_to_unknown_room.map";
+FILE24="err_no_end2.map";
+FILE25="err_no_start2.map";
+FILE26="err_start_end_same.map";
+FILE27="err_too_many_ants.map";
+FILE28="err_no_start.map";
+FILE29="err_no_ants3.map";
+FILE30="err_no_rooms.map";
+FILE31="err_letter_in_coordinate.map";
 
 echo "\n\n\033[32m$FILE\n\033[37m"
 $C $DIR$FILE | $P > $USER$FILE | leaks lem-in;
@@ -103,9 +103,6 @@ echo "\n\n\033[32m$FILE8\n\033[37m"
 $C $DIR$FILE8 | $P > $USER$FILE8 | leaks lem-in;
 echo "\x1b[36mOutput: cat $USER$FILE8\n"
 
-echo "\n\n\033[32m$FILE6\n\033[37m"
-$C $DIR$FILE6 | $P > $USER$FILE6 | leaks lem-in;
-
 echo "\n\n\033[32m$FILE9\n\033[37m"
 $C $DIR$FILE9 | $P > $USER$FILE9 | leaks lem-in;
 
@@ -116,14 +113,6 @@ echo "\x1b[36mOutput: cat $USER$FILE2\n"
 echo "\n\n\033[32m$FILE3\n\033[37m"
 $C $DIR$FILE3 | $P > $USER$FILE3 | leaks lem-in;
 echo "\x1b[36mOutput: cat $USER$FILE3\n"
-
-# echo "\n\n\033[32m$FILE4\n\033[37m"
-# $C $DIR$FILE4 | $P > $USER$FILE4;
-# echo "\x1b[36mOutput: cat $USER$FILE4\n"
-
-# echo "\n\n\033[32m$FILE5\n\033[37m"
-# $C $DIR$FILE5 | $P > $USER$FILE5;
-# echo "\x1b[36mOutput: cat $USER$FILE5\n"
 
 echo "\n\n\033[32m$FILE7\n\033[37m"
 $C $DIR$FILE7 | $P > $USER$FILE7 | leaks lem-in;
@@ -168,6 +157,30 @@ $C $DIR$FILE18 | $P > $USER$FILE18 | leaks lem-in;
 
 echo "\n\n\033[32m$FILE19\n\033[37m"
 $C $DIR$FILE19 | $P > $USER$FILE19 | leaks lem-in;
+
+echo "\n\n\033[32m$FILE24\n\033[37m"
+$C $DIR$FILE24 | $P > $USER$FILE24 | leaks lem-in;
+
+echo "\n\n\033[32m$FILE25\n\033[37m"
+$C $DIR$FILE25 | $P > $USER$FILE25 | leaks lem-in;
+
+echo "\n\n\033[32m$FILE26\n\033[37m"
+$C $DIR$FILE26 | $P > $USER$FILE26 | leaks lem-in;
+
+echo "\n\n\033[32m$FILE27\n\033[37m"
+$C $DIR$FILE27 | $P > $USER$FILE27 | leaks lem-in;
+
+echo "\n\n\033[32m$FILE28\n\033[37m"
+$C $DIR$FILE28 | $P > $USER$FILE28 | leaks lem-in;
+
+echo "\n\n\033[32m$FILE29\n\033[37m"
+$C $DIR$FILE29 | $P > $USER$FILE29 | leaks lem-in;
+
+echo "\n\n\033[32m$FILE30\n\033[37m"
+$C $DIR$FILE30 | $P > $USER$FILE30 | leaks lem-in;
+
+echo "\n\n\033[32m$FILE31\n\033[37m"
+$C $DIR$FILE31 | $P > $USER$FILE31 | leaks lem-in;
 
 echo "\n\n\033[32m/dev/urandom/\033[37m"
 $C /dev/urandom | $P > $USER-dev-urandom | leaks lem-in;
